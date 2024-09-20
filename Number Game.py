083b98c8e58d6a7af3fc2a03e124057de60ebf3d
Coeconut <andrew.coe@kingsway.school.nz>
2024-09-20 16:57:23 +1200

    Update Number Game.py
    Ver 1.1

--- Number Game.py
+++ Number Game.py
@@ -1,43 +1,44 @@
 # Guess the number
 # Author: Andrew Coe
 # Date: 20/09/2024
-# Version: 1.0
-
-
-# Different types of variables
-# Store a number
+# Version: 1.1
 
 
 import random
 
 play = True
-high_score = ()
+high_score = float('inf')
 
 while play:
-    num = random.randint(1,50)
-    max_attepts = 5
+    num = random.randint(1, 100)
+    max_attepts = 10
     attempts = 0
     guess = None
     score = None
 
-    print("Guess the Number between the numbers 1 and 50 in 5 attempts\n")
+    print("\nGuess the Number between the numbers 1 and 100 in 10 attempts\n")
 
     while guess != num and attempts < max_attepts:
         attempts += 1
-        print ("")
-        print ("Attempt",attempts)
+        print (f"Attempt",attempts)
         guess = int(input("Guess:"))
         if num > guess:
             print ("higher")
         elif num < guess:
             print ("lower")
         else:
-            print ("You win")
-            if score < high_score:
+            score = attempts
+            print ("You win, your score is",score)
+            if high_score > score:
                 high_score = score
-                print ("New High Score!")
+                print ("Your New High Score is",high_score)
 
     if guess != num:
+        print("You lose, The number was: {num}")
+
+    again=str(input("Do you want to play again, type y/n "))
+    if again == "n":
+        play = False
         print("You lose, The number was:", num)
 
     again=str(input("Do you want to play again, type y/n "))
