58d4b2a96139e2a9baf829e102156056794846ab
Coeconut <andrew.coe@kingsway.school.nz>
2024-10-25 13:31:36 +1300

    Add files via upload
    Area and Perimeter V1, works with expected values, no error handling yet

---
+++ Area & Perimeter.py
@@ -0,0 +1,14 @@
+# Finding the Area & Perimeter of a rectagle
+# Author: Andrew Coe
+# Date: 2024-10-25
+# Ver: 1.0
+
+
+width = float(input("What is the width of the rectegle: "))
+height = float(input("What is the height of the rectegle: "))
+
+area = width * height
+perimeter = 2 * (width+height)
+
+print()
+print(f"The area of the rectagle is {area} and the perimeter is {perimeter}")
